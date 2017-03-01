f144415baf701b6bd928efd753d3f2c68810d103
shreben <sergei.greben@gmail.com>
2017-03-01 18:41:57 +0300

    Added comments and echos

--- deployment.sh
+++ deployment.sh
@@ -17,10 +17,8 @@ TEMP=/tmp
 RELEASE=$ARTEFACT
 APP_DEPLOY=/opt/jboss/jboss-as/server/jenkins_labwork8/deploy/hreben.war/
 
-# Output env variables
-echo "RELEASE=$RELEASE"
-
 # Checking permissions on base directory
+echo "Checking base directory permissions..."
 if [ ! -r $BASE_DIR ]
 then 
 	printf "Cannot write to $BASE_DIR. Exiting..."
@@ -28,32 +26,39 @@ then
 fi
 
 # Checking app directory structure
+echo "Checking app directory structure..."
 if [ ! -d $BASE_DIR/$APP ]
 then
-	printf "Creating app directory structure...\n"
+	printf "Missing directories. Creating app directory structure...\n"
 	cd $BASE_DIR
 	mkdir -p $APP/{$OLD,$CUR,$DIST}
 else
 	for dir in $OLD $CUR $DIST
 	do
 		cd $BASE_DIR/$APP
-		if [ ! -d $0 ]
+		if [ ! -d $dir ]
 		then
-			mkdir $0
+			echo "Directory $dir is missing. Creating one..."
+			mkdir $dir
 		fi
 	done
 fi
 
 # Retrieve artefact
+echo "Downloading artifact..."
 cd $TEMP
 wget http://nexus/repository/siarhei-hreben-raw/$RELEASE
 
 # Deploying artefact
+echo "Deploying artefact..."
 if [ tar -tf $RELEASE ]
 then
 	rm -f $BASE_DIR/$APP/$OLD/*.tar.gz && mv $BASE_DIR/$APP/$CUR/*.tar.gz $BASE_DIR/$APP/$OLD && mv $RELEASE $BASE_DIR/$APP/$CUR/
 	rm -rf $BASE_DIR/$APP/$DIST/* && tar -zxf $BASE_DIR/$APP/$CUR/$RELEASE -C $BASE_DIR/$APP/$DIST/
-#	cp $BASE_DIR/$APP/$DIST/* $APP_DEPLOY
+	cp $BASE_DIR/$APP/$DIST/* $APP_DEPLOY
+else
+	echo "Release archive is corrupted. Exiting."
+	exit
 fi
 	
 
