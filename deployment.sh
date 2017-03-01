4a36a272190c84fc1729f5a98707d4d10ea58215
shreben <sergei.greben@gmail.com>
2017-03-01 18:17:51 +0300

    Edited if statements

--- deployment.sh
+++ deployment.sh
@@ -21,14 +21,14 @@ APP_DEPLOY=/opt/jboss/jboss-as/server/jenkins_labwork8/deploy/hreben.war/
 echo "RELEASE=$RELEASE"
 
 # Checking permissions on base directory
-if [[ ! -r $BASE_DIR ]]
+if [ ! -r $BASE_DIR ]
 then 
 	printf "Cannot write to $BASE_DIR. Exiting..."
 	exit
 fi
 
 # Checking app directory structure
-if [[ ! -d $BASE_DIR/$APP ]]
+if [ ! -d $BASE_DIR/$APP ]
 then
 	printf "Creating app directory structure...\n"
 	cd $BASE_DIR
@@ -37,7 +37,7 @@ else
 	for dir in $OLD $CUR $DIST
 	do
 		cd $BASE_DIR/$APP
-		if [[! -d $0]]
+		if [ ! -d $0 ]
 		then
 			mkdir $0
 		fi
@@ -49,7 +49,7 @@ cd $TEMP
 wget http://nexus/repository/siarhei-hreben-raw/$RELEASE
 
 # Deploying artefact
-if [[tar -tf $RELEASE &> /dev/null]]
+if [ tar -tf $RELEASE ]
 then
 	rm -f $BASE_DIR/$APP/$OLD/*.tar.gz && mv $BASE_DIR/$APP/$CUR/*.tar.gz $BASE_DIR/$APP/$OLD && mv $RELEASE $BASE_DIR/$APP/$CUR/
 	rm -rf $BASE_DIR/$APP/$DIST/* && tar -zxf $BASE_DIR/$APP/$CUR/$RELEASE -C $BASE_DIR/$APP/$DIST/
